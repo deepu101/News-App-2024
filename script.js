ed93c3ff5ce3387bb163836269c187e8314d1771
deepanshu <dk5073916@gmail.com>
2024-03-04 16:21:04 +0530

    adding Dark-Mode

--- script.js
+++ script.js
@@ -6,6 +6,14 @@ var typed = new Typed('.auto-type', {
     loop: true
   });
 
+//   Dark-Mode code 
+function myFunction() {
+    var navDark = document.querySelector('.nav-dark');
+    navDark.classList.toggle("dark-mode");
+    navDark.style.color = "white";
+    var element = document.body;
+    element.classList.toggle("dark-mode");
+ }
   
 const API_KEY = "24875c6d82af41f39bc28596d25e147b" ;
 const url = "https://newsapi.org/v2/everything?q=";
